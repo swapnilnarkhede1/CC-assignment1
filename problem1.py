709111ac71bc6af726a03c342825d71bd2f98a9e
swapnilnarkhede1 <34669001+swapnilnarkhede1@users.noreply.github.com>
2020-04-07 14:51:32 +0530

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,9 @@
+
+a=int(input("Enter the first integer  "))
+b=int(input("Enter the first integer  "))
+sum=a+b
+print(" the sum is ",sum)
+diff=a-b
+print(" the difference is ",diff)
+prod=a*b
+print("the product is",prod)
